9567d08e80b11ae695f6de8ba82768790d847622
Agustin <agustin.devfs@gmail.com>
2025-02-25 21:58:10 -0300

    font Exo next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,20 @@
-"use client"; // Importante para que este Layout sea un Client Component
+"use client";
 
 import { ReactNode } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Script from "next/script";
+import { Exo } from "next/font/google";
 
 import "./globals.css";
 import theme from "@/app/theme/index";
+
+const exo = Exo({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], 
+  style: ["normal", "italic"], 
+  subsets: ["latin"],
+  variable: "--font-exo-sans",
+});
 
 interface RootLayoutProps {
   children: ReactNode;
@@ -21,21 +29,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <title>OMegon</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-         <link
-          href="https://fonts.googleapis.com/css2?family=Exo:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        /> 
-        <Script
-          src="https://cdn.lordicon.com/bhenfmcm.js"
-          strategy="afterInteractive"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <Script src="https://cdn.lordicon.com/bhenfmcm.js" strategy="afterInteractive" />
       </head>
-      <body>
+      <body className={exo.variable}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           {children}
